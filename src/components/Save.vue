efa5f3d26db597498708f41555b3bde51d143f03
esleiter <ceo@esleiter.com>
2024-02-05 12:24:33 -0500

    Fix local storage for saved data

--- src/components/Save.vue
+++ src/components/Save.vue
@@ -17,8 +17,6 @@ defineProps(['id'])
 import { useSaveSeries } from '../stores/saveSeries';
 const saveSeries = useSaveSeries();
 
-localStorage.setItem("savesData", JSON.stringify([]));
-
 saveSeries.loadFromLocalStorage();
 </script>
 
